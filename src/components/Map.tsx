5751c779286b42ce28dc62f4c906381da385a8b8
Laurent Binder <laurentbinder@gmail.com>
2024-03-20 17:35:04 +0100

    add polygons corsica map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
+import { regionCorseGeoJson } from '../data/regionCorseGeoJson';
 
 const Map = () => {
   useEffect(() => {
@@ -9,17 +10,16 @@ const Map = () => {
       mapContainer?.querySelector('.leaflet-map-pane');
 
     if (!mapAlreadyInitialized) {
-      const map = L.map(mapContainer!).setView([48.8566, 2.3522], 13); // Centered on Paris, France
+      const map = L.map(mapContainer!).setView([42.3, 9.15], 8); // Centered on Paris, France
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([48.8584, 2.2945])
-        .addTo(map)
-        .bindPopup('Eiffel Tower')
-        .openPopup();
+      L.geoJSON(regionCorseGeoJson).addTo(map);
+
+      // L.marker([42.3, 9.15]).addTo(map).bindPopup('Corte').openPopup();
     }
   }, []);
 
